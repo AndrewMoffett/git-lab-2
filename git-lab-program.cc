389450629a3ed8a0ec3fd871c5b54786164a4eaf
Andrew <am787816@ohio.edu>
2019-09-06 15:25:57 -0400

    git-lab-program.cc

---
+++ git-lab-program.cc
@@ -0,0 +1,18 @@
+/*
+ *        File: git-lab-program.cc
+ *      Author: Andrew Moffett
+ *        Date: 9-6-19
+ * Description: This file is for the git-lab we did in the lab.
+ */
+
+#include <iostream>
+#include <iomanip>
+#include <cstdlib>
+using namespace std;
+
+
+int main(int argc, char const *argv[]) {
+
+    cout << "Hello Git!!" << endl;
+    return 0;
+}// main
